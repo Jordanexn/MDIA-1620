16dde169883517670e2fb47300c79dbb16561254
Jordane Nyirenda <jordanenyirenda@TATENDAs-MacBook-Pro.local>
2024-09-20 15:52:28 -0700

    initialize stables

---
+++ stables.js
@@ -0,0 +1,10 @@
+let horseName = "bibble";
+console.log(horseName);
+horseName = "barbie";
+console.log(horseName);
+let horseAge = 7;
+console.log(horseAge);
+
+
+let isHorseInside = true; 
+console.log(isHorseInside)
