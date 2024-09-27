3c175f728b7464bcca8f63e337014f6ba8a50c27
Jordane Nyirenda <jordanenyirenda@TATENDAs-MacBook-Pro.local>
2024-09-27 16:03:53 -0700

    added info in stables

--- stables.js
+++ stables.js
@@ -8,3 +8,31 @@ console.log(horseAge);
 
 let isHorseInside = true; 
 console.log(isHorseInside)
+
+// lesson-4 code
+console.log(typeof "888")
+console.log(typeof true)
+console.log( typeof false)
+console.log(typeof horseName)
+console.log(typeof horseAge)
+
+const STABLE_MONTHLY_FEE = 888; // uppercase is preffernce for consts to make them easier to find 
+console.log(STABLE_MONTHLY_FEE);
+
+console.log(STABLE_MONTHLY_FEE * 3);
+console.log(STABLE_MONTHLY_FEE - 444);
+console.log("bye" + "  Felica") 
+console.log("bye ")
+console.log 
+
+
+
+
+
+console.log("my horse is iridescent")
+console.log("the stable monthly fee is " + STABLE_MONTHLY_FEE) // concatenation 
+console.log()`My horse's name is +${horseName}" and costs ${STABLE_MONTHLY_FEE})
+
+
+
+
