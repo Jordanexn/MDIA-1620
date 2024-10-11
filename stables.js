5e6b7a6e3d22c84d1975929ffe5b571d73b2cff5
JordaneNyirenda <jnyirenda1@my.bcit.ca>
2024-10-11 13:38:22 -0700

    lesson-5

--- stables.js
+++ stables.js
@@ -1,10 +1,19 @@
-let horseName = "bibble";
+
+let horseName="bibble";
 console.log(horseName);
-horseName = "barbie";
+horseName="barbie";
 console.log(horseName);
-let horseAge = 7;
+let horseAge=7;
 console.log(horseAge);
 
+let isHorseInside=true; 
+console.log(isHorseInside); 
+
+
+//week 5 branch////
 
 let isHorseInside = true; 
-console.log(isHorseInside)
+console.log(isHorseInside);
+
+let isHorsOutside = false;
+console.log(isHorsOutside)
